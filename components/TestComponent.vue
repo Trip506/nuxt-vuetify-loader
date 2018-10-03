2ebb687fc703e00c5166040dd63fab0e892ae132
Chris Sheppard <chris@idleserv.net>
2018-10-03 09:40:46 +0100

    Initial

---
+++ components/TestComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>hello</h1>
+</template>
+
+
+<script>
+export default {
+  methods: {
+    render (createElement) {
+      return createElement('h1', 'Test Component')
+    }
+  }
+}
+</script>
