2355403eb55339ad5722f5ee5b80b036a4b20fbd
Chris Sheppard <chris@idleserv.net>
2018-10-03 11:48:35 +0100

    Dynamic components still have to be manually imported
    https://github.com/vuetifyjs/vuetify-loader/issues/9

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify/lib'
+import Vuetify, { VLayout } from 'vuetify/lib'
 import colors from 'vuetify/es5/util/colors'
 
 Vue.use(Vuetify, {
@@ -11,5 +11,9 @@ Vue.use(Vuetify, {
     warning: colors.amber.base,
     error: colors.deepOrange.accent4,
     success: colors.green.accent3
+  },
+
+  components: {
+    VLayout
   }
 })
