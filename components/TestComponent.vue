114ec34d53d57ad17190bb28323d91b19460c2d5
Chris Sheppard <chris@idleserv.net>
2018-10-03 11:06:13 +0100

    Show data iterator bug

--- components/TestComponent.vue
+++ components/TestComponent.vue
@@ -1,14 +1,7 @@
-<template>
-  <h1>hello</h1>
-</template>
-
-
 <script>
 export default {
-  methods: {
-    render (createElement) {
-      return createElement('h1', 'Test Component')
-    }
+  render (createElement) {
+    return createElement('h1', 'Test Component')
   }
 }
 </script>
